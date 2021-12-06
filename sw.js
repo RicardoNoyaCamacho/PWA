65c8edbced1f049cc3e747a268a8619429d866f8
Ricardo Noya Camacho <ricardo.noya@bykon.com.mx>
2021-12-05 22:48:46 -0600

    update v4

--- sw.js
+++ sw.js
@@ -5,22 +5,22 @@ const DYNAMIC_CACHE = "dynamic-v1";
 const INMUTABLE_CACHE = "inmutable-v1";
 
 const APP_SHELL = [
-  "/index.html",
-  "/css/style.css",
-  "/img/avatars/hulk.jpg",
-  "/img/avatars/ironman.jpg",
-  "/img/avatars/spiderman.jpg",
-  "/img/avatars/thor.jpg",
-  "/img/avatars/wolverine.jpg",
-  "/js/app.js",
-  "/js/sw-utils.js",
+  "index.html",
+  "css/style.css",
+  "img/avatars/hulk.jpg",
+  "img/avatars/ironman.jpg",
+  "img/avatars/spiderman.jpg",
+  "img/avatars/thor.jpg",
+  "img/avatars/wolverine.jpg",
+  "js/app.js",
+  "js/sw-utils.js",
 ];
 
 const APP_SHELL_INMUTABLE = [
   "https://fonts.googleapis.com/css?family=Quicksand:300,400",
   "https://fonts.googleapis.com/css?family=Lato:400,300",
-  "/css/animate.css",
-  "/js/libs/jquery.js",
+  "css/animate.css",
+  "js/libs/jquery.js",
   "https://kit.fontawesome.com/fe0eb226c6.js",
 ];
 
