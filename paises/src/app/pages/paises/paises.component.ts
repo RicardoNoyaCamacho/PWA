c95e4bde70bd551d5ea2387666718d7df174b0da
Ricardo Noya Camacho <ricardo.noya@bykon.com.mx>
2021-12-10 14:09:56 -0600

    all project

--- paises/src/app/pages/paises/paises.component.ts
+++ paises/src/app/pages/paises/paises.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { PaisesService } from 'src/app/services/paises.service';
+import { PaisInterface } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-paises',
   templateUrl: './paises.component.html',
-  styleUrls: ['./paises.component.css']
+  styleUrls: ['./paises.component.css'],
 })
 export class PaisesComponent implements OnInit {
+  paises: PaisInterface[] = [];
 
-  constructor() { }
+  constructor(public paisService: PaisesService) {}
 
   ngOnInit(): void {
+    this.paisService.getPaises().then((paises) => {
+      this.paises = paises;
+    });
   }
-
 }
--- paises/src/app/services/paises.service.ts
+++ paises/src/app/services/paises.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { PaisInterface } from '../interfaces/pais.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PaisesService {
+  private paises: PaisInterface[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getPaises(): Promise<PaisInterface[]> {
+    if (this.paises.length > 0) {
+      return Promise.resolve(this.paises);
+    }
+
+    return new Promise((resolve) => {
+      this.http
+        .get('https://restcountries.com/v2/lang/es')
+        .subscribe((paises: any) => {
+          console.log(paises);
+          this.paises = paises;
+          resolve(paises);
+        });
+    });
+  }
+
+  getPaisById(id: string) {
+    if (this.paises.length > 0) {
+      //Hay paises en el arreglo
+      const pais = this.paises.find((p) => p.alpha3Code === id);
+      return Promise.resolve(pais);
+    }
+
+    return this.getPaises().then((paises) => {
+      const pais = this.paises.find((p) => p.alpha3Code === id);
+      return Promise.resolve(pais);
+    });
+  }
 }
